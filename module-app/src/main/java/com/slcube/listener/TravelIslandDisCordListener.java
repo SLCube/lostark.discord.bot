694d8c646d671b71940cbd66f26da3c603ac721e
SLCube <kgm0096@naver.com>
2025-03-09 20:11:58 +0900

    feat: as-is module에 있는 discord listener 코드 이관작업

--- module-app/src/main/java/com/slcube/listener/TravelIslandDisCordListener.java
+++ module-app/src/main/java/com/slcube/listener/TravelIslandDisCordListener.java
@@ -3,9 +3,18 @@ package com.slcube.listener;
 import com.slcube.application.use_case.in.ProvideCalendarScheduleInfoUseCase;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -17,5 +26,49 @@ public class TravelIslandDisCordListener extends ListenerAdapter {
     @Value("${discord.channel.id}")
     private String targetChannelId;
 
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        if (!event.getChannel().getId().equals(targetChannelId)) return;
 
+        User user = event.getAuthor();
+        TextChannel textChannel = event.getChannel().asTextChannel();
+        Message message = event.getMessage();
+
+        if (user.isBot()) return;
+
+        String msg = message.getContentDisplay();
+        if (msg.equals("!모험섬")) {
+            String receiveMessage = "receiveMessage";
+            textChannel.sendMessage(receiveMessage).queue();
+        }
+    }
+
+    @Override
+    public void onReady(ReadyEvent event) {
+        TextChannel channel = event.getJDA().getTextChannelById(targetChannelId);
+
+        if (channel != null) {
+            long initialDelay = calculateInitialDelay();
+            long period = TimeUnit.DAYS.toMillis(1);
+
+            String receiveMessage = "receiveMessage";
+            event.getJDA().getGatewayPool().scheduleAtFixedRate(
+                    () -> channel.sendMessage(receiveMessage).queue(),
+                    initialDelay,
+                    period,
+                    TimeUnit.MILLISECONDS);
+        }
+    }
+
+    private long calculateInitialDelay() {
+        LocalDateTime now = LocalDateTime.now();
+
+        LocalDateTime targetTime = now.withHour(10).withMinute(30).withSecond(0).withNano(0);
+        if (now.isAfter(targetTime)) {
+            targetTime = targetTime.plusDays(1); // 내일 오전 10시 30분
+        }
+
+        // 현재 시간과 목표 시간 사이의 차이를 밀리초 단위로 계산
+        return ChronoUnit.MILLIS.between(now, targetTime);
+    }
 }
